67d85fcf7f9c370ef3c6bbe9334aa7c31cf10d4e
Mathew Weaver <mweaver@earthsoft.com>
2015-08-24 06:16:22 -0600

    added logging and connection string

--- Main.Designer.vb
+++ Main.Designer.vb
@@ -22,9 +22,20 @@ Partial Class Main
   'Do not modify it using the code editor.
   <System.Diagnostics.DebuggerStepThrough()> _
   Private Sub InitializeComponent()
+    Me.components = New System.ComponentModel.Container()
     Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
     Me.btnAdd = New System.Windows.Forms.Button()
+    Me.tabControl = New System.Windows.Forms.TabControl()
+    Me.tabToday = New System.Windows.Forms.TabPage()
+    Me.tabHistory = New System.Windows.Forms.TabPage()
     Me.webBrowser = New System.Windows.Forms.WebBrowser()
+    Me.toolTip = New System.Windows.Forms.ToolTip(Me.components)
+    Me.tabVolunteer = New System.Windows.Forms.TabPage()
+    Me.flowPanel = New System.Windows.Forms.FlowLayoutPanel()
+    Me.tabControl.SuspendLayout()
+    Me.tabToday.SuspendLayout()
+    Me.tabHistory.SuspendLayout()
+    Me.tabVolunteer.SuspendLayout()
     Me.SuspendLayout()
     '
     'btnAdd
@@ -32,23 +43,76 @@ Partial Class Main
     Me.btnAdd.BackColor = System.Drawing.Color.RoyalBlue
     Me.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand
     Me.btnAdd.ForeColor = System.Drawing.Color.White
-    Me.btnAdd.Location = New System.Drawing.Point(12, 12)
+    Me.btnAdd.Location = New System.Drawing.Point(18, 22)
     Me.btnAdd.Name = "btnAdd"
     Me.btnAdd.Size = New System.Drawing.Size(214, 160)
     Me.btnAdd.TabIndex = 0
     Me.btnAdd.Text = "ADD VISITOR"
     Me.btnAdd.UseVisualStyleBackColor = False
     '
+    'tabControl
+    '
+    Me.tabControl.Controls.Add(Me.tabToday)
+    Me.tabControl.Controls.Add(Me.tabHistory)
+    Me.tabControl.Controls.Add(Me.tabVolunteer)
+    Me.tabControl.Dock = System.Windows.Forms.DockStyle.Fill
+    Me.tabControl.Location = New System.Drawing.Point(0, 0)
+    Me.tabControl.Name = "tabControl"
+    Me.tabControl.SelectedIndex = 0
+    Me.tabControl.Size = New System.Drawing.Size(1012, 457)
+    Me.tabControl.TabIndex = 2
+    '
+    'tabToday
+    '
+    Me.tabToday.Controls.Add(Me.btnAdd)
+    Me.tabToday.Location = New System.Drawing.Point(4, 34)
+    Me.tabToday.Name = "tabToday"
+    Me.tabToday.Padding = New System.Windows.Forms.Padding(3)
+    Me.tabToday.Size = New System.Drawing.Size(1004, 419)
+    Me.tabToday.TabIndex = 0
+    Me.tabToday.Text = "Today"
+    Me.tabToday.ToolTipText = "Use this tab to add visitors."
+    Me.tabToday.UseVisualStyleBackColor = True
+    '
+    'tabHistory
+    '
+    Me.tabHistory.Controls.Add(Me.webBrowser)
+    Me.tabHistory.Location = New System.Drawing.Point(4, 34)
+    Me.tabHistory.Name = "tabHistory"
+    Me.tabHistory.Padding = New System.Windows.Forms.Padding(3)
+    Me.tabHistory.Size = New System.Drawing.Size(1004, 419)
+    Me.tabHistory.TabIndex = 1
+    Me.tabHistory.Text = "History"
+    Me.tabHistory.ToolTipText = "View the history of visitors"
+    Me.tabHistory.UseVisualStyleBackColor = True
+    '
     'webBrowser
     '
-    Me.webBrowser.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-    Me.webBrowser.Location = New System.Drawing.Point(232, 0)
+    Me.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill
+    Me.webBrowser.Location = New System.Drawing.Point(3, 3)
     Me.webBrowser.MinimumSize = New System.Drawing.Size(20, 20)
     Me.webBrowser.Name = "webBrowser"
-    Me.webBrowser.Size = New System.Drawing.Size(778, 454)
-    Me.webBrowser.TabIndex = 1
+    Me.webBrowser.Size = New System.Drawing.Size(998, 413)
+    Me.webBrowser.TabIndex = 2
+    '
+    'tabVolunteer
+    '
+    Me.tabVolunteer.Controls.Add(Me.flowPanel)
+    Me.tabVolunteer.Location = New System.Drawing.Point(4, 34)
+    Me.tabVolunteer.Name = "tabVolunteer"
+    Me.tabVolunteer.Size = New System.Drawing.Size(1004, 419)
+    Me.tabVolunteer.TabIndex = 2
+    Me.tabVolunteer.Text = "Volunteers"
+    Me.tabVolunteer.ToolTipText = "Check-in/check-out volunteers"
+    Me.tabVolunteer.UseVisualStyleBackColor = True
+    '
+    'flowPanel
+    '
+    Me.flowPanel.Dock = System.Windows.Forms.DockStyle.Fill
+    Me.flowPanel.Location = New System.Drawing.Point(0, 0)
+    Me.flowPanel.Name = "flowPanel"
+    Me.flowPanel.Size = New System.Drawing.Size(1004, 419)
+    Me.flowPanel.TabIndex = 0
     '
     'Main
     '
@@ -56,8 +120,7 @@ Partial Class Main
     Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
     Me.BackColor = System.Drawing.Color.White
     Me.ClientSize = New System.Drawing.Size(1012, 457)
-    Me.Controls.Add(Me.webBrowser)
-    Me.Controls.Add(Me.btnAdd)
+    Me.Controls.Add(Me.tabControl)
     Me.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
     Me.ForeColor = System.Drawing.Color.Gray
     Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -66,10 +129,20 @@ Partial Class Main
     Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
     Me.Text = "Visitor Tracker"
     Me.TopMost = True
+    Me.tabControl.ResumeLayout(False)
+    Me.tabToday.ResumeLayout(False)
+    Me.tabHistory.ResumeLayout(False)
+    Me.tabVolunteer.ResumeLayout(False)
     Me.ResumeLayout(False)
 
   End Sub
   Friend WithEvents btnAdd As System.Windows.Forms.Button
+  Friend WithEvents tabControl As System.Windows.Forms.TabControl
+  Friend WithEvents tabToday As System.Windows.Forms.TabPage
+  Friend WithEvents tabHistory As System.Windows.Forms.TabPage
   Friend WithEvents webBrowser As System.Windows.Forms.WebBrowser
+  Friend WithEvents toolTip As System.Windows.Forms.ToolTip
+  Friend WithEvents tabVolunteer As System.Windows.Forms.TabPage
+  Friend WithEvents flowPanel As System.Windows.Forms.FlowLayoutPanel
 
 End Class
--- Main.vb
+++ Main.vb
@@ -1,8 +1,40 @@
-﻿Public Class Main
+﻿Imports System
+Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Main
+
+  Protected Log As log4net.ILog = log4net.LogManager.GetLogger(GetType(Main))
+
+  Protected Connection As SqlConnection
+
+  Public Sub New()
+
+    ' This call is required by the designer.
+    InitializeComponent()
+
+    ' initialize logging
+    log4net.Config.XmlConfigurator.Configure()
+
+    ' create database connection
+    Me.Connection = New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("VisitorTracker").ConnectionString)
+
+  End Sub
 
   Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
-    Me.webBrowser.DocumentText = My.Resources.ChartTest
+    Try
+      Me.webBrowser.DocumentText = My.Resources.ChartTest
 
+      Me.Connection.Open()
+      Me.Connection.Close()
 
+    Catch ex As Exception
+      HandleException(ex)
+    End Try
+  End Sub
+
+  Private Sub HandleException(ex As Exception)
+    Log.Error(ex.Message, ex)
+    MessageBox.Show(ex.Message)
   End Sub
 End Class
